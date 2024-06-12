bcd6bed373c240d725f1f6353c06124ad5a7e1d0
khushbu-s-shah <141067682+khushbu-s-shah@users.noreply.github.com>
2024-06-11 23:49:31 -0600

    changes on Tailwind CSS for Assignment-5

--- app/week-5/item-list.js
+++ app/week-5/item-list.js
@@ -24,23 +24,23 @@ export default function ItemList() {
 
     return (
         <div className="p-4">
-            <div className="bg-blue-100 p-5 block sm:flex justify-center rounded-lg shadow-md mb-6">
-              <label htmlFor='item' className='text-lg text-gray-800 mb-4 font-bold p-2 py-3'>Sort:</label>
+            <div className="bg-black flex justify-center mb-6 mx-auto  p-1">
+              <label htmlFor='item' className='text-lg text-white mb-4 font-bold p-2 py-2 px-4'>Sort:</label>
                 <button
-                    className={`block sm:inline-block m-2 px-4 py-2 rounded-lg shadow  ${sortBy === 'name' ? 'bg-blue-500' : 'bg-gray-500'}`}
+                    className={`block sm:inline-block md:text-xl m-2 px-4 py-2 rounded-full text-white shadow  ${sortBy === 'name' ? 'bg-gradient-to-tr from-violet-400 to-green-400' : ' bg-green-400'}`}
                     onClick={() => setSortBy('name')}
                 >
                     Sort by Name
                 </button>
 
                 <button
-                    className={`block sm:inline-block m-2 px-4 py-2 rounded-lg shadow  ${sortBy === 'category' ? 'bg-blue-500' : 'bg-gray-500'}`}
+                    className={`block sm:inline-block md:text-xl m-2 px-4 py-2 rounded-full text-white shadow  ${sortBy === 'category' ? 'bg-gradient-to-tr from-violet-400 to-green-400' : 'bg-green-500'}`}
                     onClick={() => setSortBy('category')}
                 >
                     Sort by Category
                 </button>
 
-                <button className={`block sm:inline-block m-2 px-4 py-2 rounded-lg shadow  ${sortBy === 'group' ? 'bg-blue-500' : 'bg-gray-500'}`}
+                <button className={`block sm:inline-block md:text-xl m-2 px-4 py-2 rounded-full text-white shadow  ${sortBy === 'group' ? 'bg-gradient-to-tr from-violet-400 to-green-400' : 'bg-green-500'}`}
                 onClick={() => setSortBy('group')}>
                     Group by Category
                 </button>
@@ -51,7 +51,7 @@ export default function ItemList() {
          <div className="flex flex-col md:flex-row md:flex-wrap">
           {Object.keys(groupedItems).map(category => (
             <div key={category} className="w-full md:w-1/2 lg:w-1/3 p-2">
-              <h2 className="capitalize text-xl font-semibold mt-4 bg-gray-200 p-2 rounded">{category}</h2>
+              <h2 className="capitalize text-xl font-semibold mt-4 bg-gray-400 p-2 rounded">{category}</h2>
               <ul>
                 {groupedItems[category].map(item => (
                   <Item
